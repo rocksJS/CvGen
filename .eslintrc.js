1d262073fe661c4571bfc9ff289cf4217b28b8a2
rocksJS <6abyghost@gmail.com>
2023-01-23 18:05:40 +0300

    +prettier, eslint, and his settings

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "@angular-eslint"],
+  rules: {
+    "import/no-extraneous-dependencies": 0,
+    "no-empty-function": ["error", { allow: ["constructors"] }],
+    "@angular-eslint/no-empty-lifecycle-method": "error",
+    "import/named": "off",
+    "no-unused-expressions": "off",
+  },
+};
