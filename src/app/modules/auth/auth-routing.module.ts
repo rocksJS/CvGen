945e2b31f51564a7986ae6f2879f9b3f514ddb0e
rocksJS <6abyghost@gmail.com>
2023-01-29 11:15:32 +0300

    testing precommit another one

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AuthPageComponent } from './pages/auth-page/auth-page.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AuthPageComponent } from "./pages/auth-page/auth-page.component";
 
-const routes: Routes = [{ path: '', component: AuthPageComponent }];
+const routes: Routes = [{ path: "", component: AuthPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class AuthRoutingModule {}
+export class AuthRoutingModule {
+  dwdwaawdkjdwak() {
+    console.log("testing");
+  }
+}
