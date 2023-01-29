ce399788d6c5c1d6bd65b622a0a1c19c592c4f16
rocksJS <6abyghost@gmail.com>
2023-01-29 18:36:10 +0300

    done sidebar, update stylelint

--- src/app/modules/main/components/sidebar/sidebar.component.ts
+++ src/app/modules/main/components/sidebar/sidebar.component.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
-  selector: 'cvg-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss'],
+  selector: "cvg-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  isCollapsed = false;
+
+  toggleCollapsed(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
